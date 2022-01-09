d8728c097d30ab71186ae3609a69e9a966275dd4
Adrian Kunz <clashsoft@hotmail.com>
2022-01-09 17:01:50 +0100

    feat: Configurable top position

--- projects/ng-bootstrap-ext/src/lib/toast/toast-list/toast-list.component.ts
+++ projects/ng-bootstrap-ext/src/lib/toast/toast-list/toast-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {ToastService} from '../toast.service';
 
 @Component({
@@ -7,6 +7,7 @@ import {ToastService} from '../toast.service';
   styleUrls: ['./toast-list.component.scss'],
 })
 export class ToastListComponent implements OnInit {
+  @Input() top: string | number = 0;
 
   constructor(
     public toastService: ToastService,
